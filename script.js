0fab50d7d15483255fe8a0b4c939f5fa0aa38aef
fataly27 <fataly27@gmail.com>
2016-11-19 12:34:13 +0100

    Ajout des scripts des navs

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function() {
+	$('nav ul li a').click(function()
+	{
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+		{
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length)
+			{
+				$('html, body').animate(
+				{
+					scrollTop: target.offset().top - $('#fixed-nav').outerHeight(true)
+					
+				}, 700);
+				return false;
+			}
+		}
+	});
+	
+	$(window).scroll(showOrHide);
+	
+	showOrHide();
+});
+
+function showOrHide()
+{
+	if ($('#static-nav').offset().top - $(window).scrollTop() < 0)
+			$('#fixed-nav').show();
+	else
+		$('#fixed-nav').hide();
+}
