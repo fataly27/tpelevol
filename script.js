ede240cb29db39f27dab37f95c0cbf196c1cd96b
fataly27 <fataly27@gmail.com>
2016-12-04 12:58:45 +0100

    Remise à plat

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ function showOrHide()
 	else
 		$('#fixed-nav').hide();
 }
+
+$("video").hover(function()
+{
+  $(this).prop("controls", true);
+  
+}, function()
+{
+	$(this).prop("controls", false);
+
+});
+
+$("video").click(function()
+{
+	if( this.paused)
+		this.play();
+	else
+		this.pause();
+});
